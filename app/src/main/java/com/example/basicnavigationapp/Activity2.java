2a45ad0d9b6bc2b865f3c6c2c13e1c206ddec690
maxmccomb <maxwellmccomb16@augustana.edu>
2020-02-26 18:50:51 -0600

    Added functionality to... -move between two activities. -pass text between two activities

--- app/src/main/java/com/example/basicnavigationapp/Activity2.java
+++ app/src/main/java/com/example/basicnavigationapp/Activity2.java
@@ -3,12 +3,35 @@ package com.example.basicnavigationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-
+    String enteredText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        textView = (TextView) findViewById(R.id.textView2);
+        enteredText = getIntent().getExtras().getString("Value");
+        textView.setText(enteredText);
+
+        configureBackButton();
+    }
+
+
+
+    public void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/basicnavigationapp/MainActivity.java
+++ app/src/main/java/com/example/basicnavigationapp/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.basicnavigationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String enteredText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureSubmitButton();
+    }
+
+    public void configureSubmitButton(){
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                EditText et = findViewById(R.id.editText);
+                enteredText = et.getText().toString();
+                i.putExtra("Value", enteredText);
+                startActivity(i);
+            }
+        });
+
     }
 }
